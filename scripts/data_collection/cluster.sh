a687c98f1d7664988f907006e46040b872ffe726
AmirZur <amir.zur@hotmail.com>
2025-10-22 17:16:52 -0700

    try again with cluster run?

--- scripts/data_collection/cluster.sh
+++ scripts/data_collection/cluster.sh
@@ -1,6 +1,6 @@
 # llama
-nlprun -q jag -g 1 -r 120G -o logs/collect/llama_3b_a.log 'bash scripts/data_collection/llama_3b.sh AQuA,GPQA,GSM8k,MATH'
-nlprun -q jag -g 1 -r 120G -o logs/collect/llama_3b_b.log 'bash scripts/data_collection/llama_3b.sh PythonIO,WildJailBreak,Just-Eval'
+nlprun -q jag -g 1 -r 80G -o logs/collect/llama_3b_a.log 'bash scripts/data_collection/llama_3b.sh AQuA,GPQA,GSM8k,MATH'
+nlprun -q jag -g 1 -r 80G -o logs/collect/llama_3b_b.log 'bash scripts/data_collection/llama_3b.sh PythonIO,WildJailBreak,Just-Eval'
 # deepseek
-nlprun -q jag -g 1 -r 120G -o logs/collect/deepseek_1b_a.log 'bash scripts/data_collection/deepseek_1b.sh AQuA,GPQA,GSM8k,MATH'
-nlprun -q jag -g 1 -r 120G -o logs/collect/deepseek_1b_b.log 'bash scripts/data_collection/deepseek_1b.sh PythonIO,WildJailBreak,Just-Eval'
+nlprun -q jag -g 1 -r 80G -o logs/collect/deepseek_1b_a.log 'bash scripts/data_collection/deepseek_1b.sh AQuA,GPQA,GSM8k,MATH'
+nlprun -q jag -g 1 -r 80G -o logs/collect/deepseek_1b_b.log 'bash scripts/data_collection/deepseek_1b.sh PythonIO,WildJailBreak,Just-Eval'
