3f50fc10d7531a001923cff3fdf14e532f6fb58e
AmirZur <amir.zur@hotmail.com>
2025-10-24 17:53:01 -0700

    what's up with the cpu parameter

--- scripts/data_collection/cluster.sh
+++ scripts/data_collection/cluster.sh
@@ -1,6 +1,6 @@
 # llama
-nlprun -m jagupard39 -g 1 -r 80G -o logs/collect/llama_3b_a.log 'bash scripts/data_collection/llama_3b.sh AQuA,GPQA,GSM8k,MATH'
-nlprun -m jagupard39 -g 1 -r 80G -o logs/collect/llama_3b_b.log 'bash scripts/data_collection/llama_3b.sh PythonIO,WildJailBreak,Just-Eval'
+nlprun -m jagupard39 -g 1 -c 3 -r 80G -o logs/collect/llama_3b_a.log 'bash scripts/data_collection/llama_3b.sh AQuA,GPQA,GSM8k,MATH'
+nlprun -m jagupard39 -g 1 -c 3 -r 80G -o logs/collect/llama_3b_b.log 'bash scripts/data_collection/llama_3b.sh PythonIO,WildJailBreak,Just-Eval'
 # deepseek
-nlprun -m jagupard33 -g 1 -r 80G -o logs/collect/deepseek_1b_a.log 'bash scripts/data_collection/deepseek_1b.sh AQuA,GPQA,GSM8k,MATH'
-nlprun -m jagupard33 -g 1 -r 80G -o logs/collect/deepseek_1b_b.log 'bash scripts/data_collection/deepseek_1b.sh PythonIO,WildJailBreak,Just-Eval'
+nlprun -m jagupard33 -g 1 -c 3 -r 80G -o logs/collect/deepseek_1b_a.log 'bash scripts/data_collection/deepseek_1b.sh AQuA,GPQA,GSM8k,MATH'
+nlprun -m jagupard33 -g 1 -c 3 -r 80G -o logs/collect/deepseek_1b_b.log 'bash scripts/data_collection/deepseek_1b.sh PythonIO,WildJailBreak,Just-Eval'
