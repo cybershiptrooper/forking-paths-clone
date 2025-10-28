ece44bb1d8caeb4b3c4221b63246bfd78c8ae260
AmirZur <amir.zur@hotmail.com>
2025-10-28 09:16:43 -0700

    swap device

--- scripts/forking_paths/cluster.sh
+++ scripts/forking_paths/cluster.sh
@@ -1,9 +1,9 @@
 # llama
-nlprun -m jagupard36 -g 1 -r 80G -c 3 -o logs/fork/llama_3b_aqua.log 'bash scripts/forking_paths/llama_3b.sh AQuA'
-nlprun -m jagupard36 -g 1 -r 80G -c 3 -o logs/fork/llama_3b_gpqa.log 'bash scripts/forking_paths/llama_3b.sh GPQA'
-nlprun -m jagupard36 -g 1 -r 80G -c 3 -o logs/fork/llama_3b_gsm8k.log 'bash scripts/forking_paths/llama_3b.sh GSM8k'
-nlprun -m jagupard36 -g 1 -r 80G -c 3 -o logs/fork/llama_3b_math.log 'bash scripts/forking_paths/llama_3b.sh MATH'
-nlprun -m jagupard36 -g 1 -r 80G -c 3 -o logs/fork/llama_3b_wildjailbreak.log 'bash scripts/forking_paths/llama_3b.sh WildJailBreak'
+nlprun -m jagupard39 -g 1 -r 80G -c 3 -o logs/fork/llama_3b_aqua.log 'bash scripts/forking_paths/llama_3b.sh AQuA'
+nlprun -m jagupard39 -g 1 -r 80G -c 3 -o logs/fork/llama_3b_gpqa.log 'bash scripts/forking_paths/llama_3b.sh GPQA'
+nlprun -m jagupard39 -g 1 -r 80G -c 3 -o logs/fork/llama_3b_gsm8k.log 'bash scripts/forking_paths/llama_3b.sh GSM8k'
+nlprun -m jagupard37 -g 1 -r 80G -c 3 -o logs/fork/llama_3b_math.log 'bash scripts/forking_paths/llama_3b.sh MATH'
+nlprun -m jagupard37 -g 1 -r 80G -c 3 -o logs/fork/llama_3b_wildjailbreak.log 'bash scripts/forking_paths/llama_3b.sh WildJailBreak'
 # deepseek
 nlprun -m jagupard38 -g 1 -r 80G -c 3 -o logs/fork/deepseek_1b_aqua.log 'bash scripts/forking_paths/deepseek_1b.sh AQuA'
 nlprun -m jagupard38 -g 1 -r 80G -c 3 -o logs/fork/deepseek_1b_gpqa.log 'bash scripts/forking_paths/deepseek_1b.sh GPQA'
