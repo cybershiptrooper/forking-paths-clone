55027a7778aaade5364e0e1a6b7172c9d7a3d32b
AmirZur <amir.zur@hotmail.com>
2025-10-27 17:44:11 -0700

    try different machine?

--- scripts/forking_paths/cluster.sh
+++ scripts/forking_paths/cluster.sh
@@ -1,11 +1,11 @@
 # llama
-nlprun -m jagupard36 -g 1 -r 80G -c 3 -o logs/fork/llama_3b_aqua.log 'bash scripts/forking_paths/llama_3b.sh AQuA'
+nlprun -m jagupard38 -g 1 -r 80G -c 3 -o logs/fork/llama_3b_aqua.log 'bash scripts/forking_paths/llama_3b.sh AQuA'
 # nlprun -q jag -g 1 -r 80G -c 3 -o logs/fork/llama_3b_gpqa.log 'bash scripts/forking_paths/llama_3b.sh GPQA'
 # nlprun -q jag -g 1 -r 80G -c 3 -o logs/fork/llama_3b_gsm8k.log 'bash scripts/forking_paths/llama_3b.sh GSM8k'
-nlprun -m jagupard36 -g 1 -r 80G -c 3 -o logs/fork/llama_3b_math.log 'bash scripts/forking_paths/llama_3b.sh MATH'
+nlprun -m jagupard38 -g 1 -r 80G -c 3 -o logs/fork/llama_3b_math.log 'bash scripts/forking_paths/llama_3b.sh MATH'
 # nlprun -q jag -g 1 -r 80G -c 3 -o logs/fork/llama_3b_pythonio.log 'bash scripts/forking_paths/llama_3b.sh PythonIO'
 # nlprun -q jag -g 1 -r 80G -c 3 -o logs/fork/llama_3b_wildjailbreak.log 'bash scripts/forking_paths/llama_3b.sh WildJailBreak'
 # nlprun -q jag -g 1 -r 80G -c 3 -o logs/fork/llama_3b_justeval.log 'bash scripts/forking_paths/llama_3b.sh Just-Eval'
 # deepseek
-nlprun -m jagupard36 -g 1 -r 80G -c 3 -o logs/fork/deepseek_1b_aqua.log 'bash scripts/forking_paths/deepseek_1b.sh AQuA'
-nlprun -m jagupard36 -g 1 -r 80G -c 3 -o logs/fork/deepseek_1b_math.log 'bash scripts/forking_paths/deepseek_1b.sh MATH'
+nlprun -m jagupard38 -g 1 -r 80G -c 3 -o logs/fork/deepseek_1b_aqua.log 'bash scripts/forking_paths/deepseek_1b.sh AQuA'
+nlprun -m jagupard38 -g 1 -r 80G -c 3 -o logs/fork/deepseek_1b_math.log 'bash scripts/forking_paths/deepseek_1b.sh MATH'
