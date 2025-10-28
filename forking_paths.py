f625d89b6e91b2e381e15601ec393747c2fa87dd
AmirZur <amir.zur@hotmail.com>
2025-10-27 17:40:10 -0700

    debug silly file naming

--- forking_paths.py
+++ forking_paths.py
@@ -122,7 +122,7 @@ def main(
 
     with open('config.json') as f:
         config = json.load(f)
-        data_dir = config["save_locations"]["data_folder"] # input
+        data_dir = config["save_locations"]["selection_folder"] # input
         forking_paths_dir = config["save_locations"]["forking_paths_folder"] # output
         answer_model_name = config["experiment_parameters"]["answer_model"]
 
