5fb31963ae9037ebf541947fa665ae9e1f2d176d
AmirZur <amir.zur@hotmail.com>
2025-10-28 08:46:58 -0700

    screw it, release them all

--- scripts/forking_paths/deepseek_1b.sh
+++ scripts/forking_paths/deepseek_1b.sh
@@ -3,7 +3,7 @@ name=$1
 uv run python forking_paths.py \
     --model_name deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B \
     --dataset_name $name \
-    --dataset_size 10 \
+    --dataset_size 20 \
     --num_branches 20 \
     --max_new_tokens 10000 \
     --temperature 0.7 \
--- scripts/forking_paths/llama_3b.sh
+++ scripts/forking_paths/llama_3b.sh
@@ -3,7 +3,7 @@ name=$1
 uv run python forking_paths.py \
     --model_name meta-llama/Llama-3.2-3B-Instruct \
     --dataset_name $name \
-    --dataset_size 10 \
+    --dataset_size 20 \
     --num_branches 30 \
     --max_new_tokens 3000 \
     --temperature 0.7 \
