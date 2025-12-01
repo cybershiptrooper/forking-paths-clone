ab0309dadb172ea1041370db7b396ccbaa35def0
AmirZur <amir.zur@hotmail.com>
2025-12-01 13:50:11 -0800

    search over layers for probing

--- scripts/probing/deepseek_1b.sh
+++ scripts/probing/deepseek_1b.sh
@@ -1,13 +1,16 @@
 name=$1
 
-uv run python probing.py \
-    --model_name deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B \
-    --dataset_name $name \
-    --layer 22 \
-    --test_split 0.1 \
-    --cross_val_split 10 \
-    --epochs 500 \
-    --early_stopping \
-    --patience 10 \
-    --learning_rate 0.001 \
-    --seed 42
+for layer in 12 16 20 24
+do
+    uv run python probing.py \
+        --model_name deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B \
+        --dataset_name $name \
+        --layer $layer \
+        --test_split 0.1 \
+        --cross_val_split 10 \
+        --epochs 500 \
+        --early_stopping \
+        --patience 10 \
+        --learning_rate 0.001 \
+        --seed 42
+done
